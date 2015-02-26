2f17f2db9228dafb1d16495dfeb03b9ff70fab8e
Dale Pepper <dalepepper@Dales-MacBook-Pro.local>
2015-02-26 08:03:39 +0000

    Added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+
+//Plugins
+var sass = require('gulp-sass');
+var less = require('gulp-less');
+
+gulp.task('sass', function () {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'));
+    }
+)
+
+gulp.task('less', function () {
+    return gulp.src('less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('css'));
+    }
+)
